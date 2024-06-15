4d4b56f526de7d586c530beb66fe21b409835c59
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-14 21:04:22 -0700

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <div className='w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100'>
+          <div className='h-24'>
+            <Navbar />
+          </div>
+          <div className={`h-[calc(100vh-6rem)]`}>
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
