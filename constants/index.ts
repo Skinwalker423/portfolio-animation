4a20f7ee5e03ac6c87716ca83db494bf594b6283
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-15 20:05:24 -0700

    constants links

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    url: "/",
+    title: "Home",
+  },
+  {
+    url: "/about",
+    title: "About",
+  },
+  {
+    url: "/portfolio",
+    title: "Potfolio",
+  },
+  {
+    url: "/contact",
+    title: "Contact",
+  },
+];
