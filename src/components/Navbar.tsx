cdd95202167b71afc69cdfa894766e3da44ac885
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-18 22:07:48 -0700

    menu icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/menubar";
 
 import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import { links } from "../../constants";
 
 export const Navbar = () => {
   return (
@@ -29,12 +31,31 @@ export const Navbar = () => {
       </div>
       {/* responsive user menu */}
 
-      <Menubar className='p-0'>
+      <div>
+        <button className='w-10 h-8 flex flex-col justify-between z-50 relative'>
+          <Menu className='z-50' color='white' size={40} />
+          {/* <div className='w-10 h-1 bg-white rounded'></div>
+          <div className='w-10 h-1 bg-white rounded'></div>
+          <div className='w-10 h-1 bg-white rounded'></div> */}
+        </button>
+
+        <div className='absolute top-0 left-0 w-screen h-screen bg-black flex flex-col items-center justify-center gap-8 text-4xl text-white'>
+          {links.map(({ title, url }) => {
+            return (
+              <Link href={url} key={title}>
+                {title}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* <Menubar className='p-0'>
         <MenubarMenu>
           <MenubarTrigger className='w-full border-none'>
             <Menu size={32} />
           </MenubarTrigger>
-          <MenubarContent>
+          <MenubarContent className='absolute top-0 left-0 w-screen h-screen bg-black flex flex-col items-center justify-center gap-8 text-4xl '>
             <MenubarItem>
               New Tab <MenubarShortcut>⌘T</MenubarShortcut>
             </MenubarItem>
@@ -45,7 +66,7 @@ export const Navbar = () => {
             <MenubarItem>Print</MenubarItem>
           </MenubarContent>
         </MenubarMenu>
-      </Menubar>
+      </Menubar> */}
     </div>
   );
 };
