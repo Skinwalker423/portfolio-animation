a1d0d93081c78262fa84fb29e8e22fb2338f37dc
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-19 21:14:40 -0700

    Navigation links

---
+++ src/components/navbar/NavigationLinks.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+
+import Link from "next/link";
+
+import { links } from "../../../constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+export const NavigationLinks = () => {
+  const path = usePathname();
+
+  console.log("path", path);
+
+  return (
+    <nav className='hidden md:flex justify-start'>
+      <ul className='flex gap-8 items-center'>
+        {links.map(({ title, url }) => {
+          return (
+            <Link
+              className={cn(
+                "px-2 py-1",
+                path === url &&
+                  "bg-black text-white rounded scale-110"
+              )}
+              key={title}
+              href={url}
+            >
+              <li>{title}</li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
