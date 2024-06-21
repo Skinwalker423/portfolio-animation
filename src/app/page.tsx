d9296a212bffe6a41ea174c7ca10162aff9d7b05
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-20 21:30:33 -0700

    hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-foreground'>Test</h1>
+    <div className='flex flex-col max-w-6xl h-full mx-auto md:flex-row'>
+      <div className='relative flex-1 w-full'>
+        <Image
+          src={"/hero.png"}
+          alt='hero'
+          fill
+          className='object-contain'
+        />
+      </div>
+      <div className='flex-1 w-full'>Text container</div>
     </div>
   );
 }
