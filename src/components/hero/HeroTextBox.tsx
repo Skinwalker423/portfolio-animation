4e9276dd4582f33db80ca70cf83b9ca58a0767c8
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-21 00:04:13 -0700

    hero text box comp

---
+++ src/components/hero/HeroTextBox.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+interface HeroTextBoxProps {
+  title?: string;
+  description?: string;
+  buttonTitleOne?: string;
+  buttonTitleTwo?: string;
+}
+
+export const HeroTextBox = ({
+  title = "Crafting Digital Experiences, Designing Tomorrow.",
+  description = `Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Vel, quis earum. Eos laboriosam soluta quae
+        illo molestiae, sapiente quidem dignissimos
+        praesentium quo! Sint totam omnis, necessitatibus
+        mollitia illo optio ipsa!`,
+  buttonTitleOne = "Learn more",
+  buttonTitleTwo = "Get started",
+}: HeroTextBoxProps) => {
+  return (
+    <div className='flex flex-col flex-1 w-full px-2 justify-center gap-8'>
+      <h1 className='text-4xl lg:text-6xl font-bold'>
+        {title}
+      </h1>
+      <p className='text-clip lg:text-xl'>{description}</p>
+      <div className='space-x-3'>
+        <Button className='ring-1' variant={"outline"}>
+          {buttonTitleOne}
+        </Button>
+        <Button>{buttonTitleTwo}</Button>
+      </div>
+    </div>
+  );
+};
