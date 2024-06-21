98d2a296e48262a527344f677b61157ccb2496d4
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-20 21:52:06 -0700

    text container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,7 +12,24 @@ export default function Home() {
           className='object-contain'
         />
       </div>
-      <div className='flex-1 w-full'>Text container</div>
+      <div className='flex flex-1 w-full flex-col px-2 justify-center items-center gap-8'>
+        <h1 className='text-3xl font-bold text-center'>
+          Hero title
+        </h1>
+        <p className='text-clip'>
+          Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Vel, quis earum. Eos laboriosam
+          soluta quae illo molestiae, sapiente quidem
+          dignissimos praesentium quo! Sint totam omnis,
+          necessitatibus mollitia illo optio ipsa!
+        </p>
+        <div className='space-x-3'>
+          <Button variant={"outline"}>Learn more</Button>
+          <Button className='bg-secondary text-secondary-foreground'>
+            Get started
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
