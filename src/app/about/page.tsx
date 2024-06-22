a7ff18b6bfe3381c6c44c020f7a0b6b3d7e3f1ec
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-21 21:40:15 -0700

    testing about page animations

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,21 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className='h-full flex items-center justify-center'>
+      <motion.div
+        initial={{ x: -100 }}
+        animate={{ x: 100, y: 300, opacity: 0 }}
+        transition={{ bounce: 1, duration: 2, delay: 2 }}
+        className='w-96 h-96 bg-red-400 rounded'
+      >
+        AboutPage
+      </motion.div>
+      ;
+    </div>
+  );
 };
 
 export default AboutPage;
