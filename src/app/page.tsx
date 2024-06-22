42de1ae2ae8566e9f69d24154f87365e1104c384
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-21 21:15:49 -0700

    applied new hero img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className='max-w-6xl h-full mx-auto'>
+    <div className='max-w-7xl h-full mx-auto'>
       <HeroSection />
     </div>
   );
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -4,8 +4,8 @@ import { HeroTextBox } from "./HeroTextBox";
 
 export const HeroSection = () => {
   return (
-    <div className='flex flex-col w-full h-full md:flex-row'>
-      <HeroImage />
+    <div className='flex flex-col w-full h-full md:flex-row gap-4'>
+      <HeroImage src='/hero2.png' alt='winter is coming' />
       <HeroTextBox />
     </div>
   );
