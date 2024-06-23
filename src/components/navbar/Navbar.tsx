162080bac2f0eeff07f4aba1594c39bb0ae714fe
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-22 21:42:58 -0700

    padding

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { NavigationLinks } from "./NavigationLinks";
 
 export const Navbar = () => {
   return (
-    <div className='h-full flex justify-between items-center max-w-6xl mx-auto px-4 sm:px-8 md:px-12 xl:px-0 lg:grid lg:grid-cols-3'>
+    <div className='h-full flex justify-between items-center max-w-6xl mx-auto px-4 sm:px-8 md:px-12 xl:px-0 lg:grid lg:grid-cols-3 max-md:px-10'>
       <NavigationLinks />
       <Logo />
       <Socials />
