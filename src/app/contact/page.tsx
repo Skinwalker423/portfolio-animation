17492ed190c7407d6166de45231d5aa69bf2969b
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-25 10:44:54 -0700

    transition hero

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,22 @@
+import { PageTransition } from "@/components/PageTransition";
+import Image from "next/image";
 import React from "react";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <PageTransition>
+      <div className='max-w-7xl mx-auto h-screen flex flex-col justify-start items-center '>
+        <h1 className='text-8xl'>Contact Me</h1>
+        <Image
+          src={"/hero2.png"}
+          alt='hero alt'
+          width={500}
+          height={500}
+          priority
+        />
+      </div>
+    </PageTransition>
+  );
 };
 
 export default ContactPage;
