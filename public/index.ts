fba9c5989d4cd241cd1914e57b1f139eb16762eb
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-26 11:01:47 -0700

    added signature and index exports

---
+++ public/index.ts
@@ -0,0 +1,21 @@
+import dribbble from "./dribbble.png";
+import facebook from "./facebook.png";
+import github from "./github.png";
+import hero from "./hero.png";
+import hero2 from "./hero2.png";
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+import pinterest from "./pinterest.png";
+import signature from "./signature.svg";
+
+export {
+  dribbble,
+  facebook,
+  github,
+  hero,
+  hero2,
+  instagram,
+  linkedin,
+  pinterest,
+  signature,
+};
