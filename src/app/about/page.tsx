4da477cc5e48f55d0187d0f7f3b4f089d7cf5a4e
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-26 12:15:03 -0700

    scroll down icon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { type Variants, motion } from "framer-motion";
 import { PageTransition } from "@/components/PageTransition";
+import { signature } from "../../../public";
+import Image from "next/image";
+import { CircleArrowDown } from "lucide-react";
 
 const AboutPage = () => {
   // const ref = useRef(null);
@@ -9,7 +12,7 @@ const AboutPage = () => {
 
   const variants: Variants | undefined = {
     variant1: {
-      x: 300,
+      x: 500,
       opacity: 0,
       rotate: 180,
       transition: {
@@ -20,8 +23,8 @@ const AboutPage = () => {
       },
     },
     variant2: {
-      x: 100,
-      y: 100,
+      x: 200,
+      y: 200,
       rotate: 45,
       transition: { duration: 4 },
     },
@@ -29,22 +32,47 @@ const AboutPage = () => {
 
   return (
     <PageTransition>
-      <div className='h-full flex items-center justify-center'>
-        <motion.div
-          initial={{ x: -100 }}
-          animate={"variant1"}
-          variants={variants}
-          transition={{ bounce: 1, duration: 2, delay: 2 }}
-          className='w-96 h-40 bg-red-400 rounded'
-        >
-          AboutPage
-        </motion.div>
-        {/* <motion.div
-        ref={ref}
-        className='w-40 h-40 rounded-full bg-blue-400'
-        style={{ x, y }}
-      /> */}
-        ;
+      <div className='max-w-7xl mx-auto flex flex-col'>
+        <div className='flex md:grid md:grid-cols-2'>
+          {/* text */}
+          <div className='flex flex-col'>
+            <div className='flex flex-col gap-12 justify-center'>
+              <h1 className='text-2xl font-bold'>
+                BIOGRAPHY
+              </h1>
+              <p className='text-lg'>
+                Lorem, ipsum dolor sit amet consectetur
+                adipisicing elit. Possimus magni blanditiis
+                numquam perferendis consequuntur? Quibusdam
+                autem eaque, rem culpa est enim eligendi
+                molestiae incidunt explicabo sint odio
+                numquam qui voluptatem?
+              </p>
+              <span className='italic'>
+                Lorem, ipsum dolor sit amet consectetur
+                adipisicing elit.
+              </span>
+              <div className='self-end'>
+                <Image
+                  src={signature}
+                  alt={"signature"}
+                  width={185}
+                  height={77}
+                />
+              </div>
+              <div>
+                <CircleArrowDown
+                  className='stroke-red-500'
+                  size={64}
+                />
+              </div>
+            </div>
+            <div>SKILLS</div>
+            <div>EXPERIENCE</div>
+          </div>
+          {/* image */}
+          <div className='hidden md:block'>image</div>
+        </div>
       </div>
     </PageTransition>
   );
