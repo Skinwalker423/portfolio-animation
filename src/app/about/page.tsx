31fb932894c9b38b659ccae11ed38a458df17abf
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-27 09:51:56 -0700

    timeline

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -37,7 +37,7 @@ const AboutPage = () => {
       <div className='flex flex-col'>
         <div className='flex md:grid md:grid-cols-2'>
           {/* text */}
-          <div className='flex flex-col p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 gap-24 md:gap-32 lg:gap-48 xl:gap-64'>
+          <div className='flex flex-col p-4 gap-24 md:gap-32 lg:gap-48 xl:gap-64'>
             <div className='flex flex-col gap-12 justify-center'>
               <h1 className='text-2xl font-bold'>
                 BIOGRAPHY
@@ -92,12 +92,114 @@ const AboutPage = () => {
             </div>
             <div
               id='experience'
-              className='flex flex-col gap-12 justify-center'
+              className='flex flex-col justify-center'
             >
               <h1 className='text-2xl font-bold'>
                 EXPERIENCE
               </h1>
-              <div>xp list</div>
+              <div className='flex'>
+                {/* left */}
+                <div className='flex flex-col py-4 flex-1'>
+                  <div className='flex flex-col gap-6'>
+                    <h2 className='bg-white text-black px-2 py-3 font-bold text-2xl rounded-lg text-center text-clip'>
+                      Freelance Web Developer
+                    </h2>
+                    <p className='italic text-clip'>
+                      Lorem ipsum dolor, sit amet
+                      consectetur adipisicing elit.
+                      Cupiditate, et! Lorem ipsum dolor, sit
+                      amet consectetur adipisicing elit.
+                      Cupiditate, et!
+                    </p>
+                    <span className='text-red-400'>
+                      2022 - present
+                    </span>
+                    <span>
+                      <Badge className='bg-white text-black hover:text-white hover:bg-black/60'>
+                        Apple
+                      </Badge>
+                    </span>
+                  </div>
+                </div>
+                {/* center */}
+
+                <div className='flex flex-col items-center mx-2 lg:mx-10 mt-4 flex-shrink-0'>
+                  <div className='bg-white ring-4 ring-red-500 rounded-full w-4 h-4 md:w-6 md:h-6 '></div>
+                  <div className='w-2 bg-black h-full'></div>
+                </div>
+
+                {/* right */}
+                <div className='h-full w-full border flex-1'></div>
+              </div>
+              <div className='flex'>
+                {/* left */}
+                <div className='h-full w-full flex-1 border'></div>
+
+                {/* center */}
+
+                <div className='flex flex-col items-center mx-2 lg:mx-10 flex-shrink-0'>
+                  <div className='bg-white ring-4 ring-red-500 rounded-full w-4 h-4 md:w-6 md:h-6 '></div>
+                  <div className='w-2 bg-black h-full'></div>
+                </div>
+
+                {/* right */}
+                <div className='flex flex-col flex-1'>
+                  <div className='flex flex-col gap-6'>
+                    <h2 className='bg-white text-black px-2 py-3 font-bold text-2xl rounded-lg text-center text-clip'>
+                      Freelance Web Developer
+                    </h2>
+                    <p className='italic text-clip'>
+                      Lorem ipsum dolor, sit amet
+                      consectetur adipisicing elit.
+                      Cupiditate, et! Lorem ipsum dolor, sit
+                      amet consectetur adipisicing elit.
+                      Cupiditate, et!
+                    </p>
+                    <span className='text-red-400'>
+                      2022 - present
+                    </span>
+                    <span>
+                      <Badge className='bg-white text-black hover:text-white hover:bg-black/60'>
+                        Apple
+                      </Badge>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className='flex'>
+                {/* left */}
+                <div className='flex flex-col flex-1'>
+                  <div className='flex flex-col gap-6'>
+                    <h2 className='bg-white text-black px-2 py-3 font-bold text-2xl rounded-lg text-center text-clip'>
+                      Freelance Web Developer
+                    </h2>
+                    <p className='italic text-clip'>
+                      Lorem ipsum dolor, sit amet
+                      consectetur adipisicing elit.
+                      Cupiditate, et! Lorem ipsum dolor, sit
+                      amet consectetur adipisicing elit.
+                      Cupiditate, et!
+                    </p>
+                    <span className='text-red-400'>
+                      2022 - present
+                    </span>
+                    <span>
+                      <Badge className='bg-white text-black hover:text-white hover:bg-black/60'>
+                        Apple
+                      </Badge>
+                    </span>
+                  </div>
+                </div>
+                {/* center */}
+
+                <div className='flex flex-col items-center mx-2 lg:mx-10 flex-shrink-0'>
+                  <div className='bg-white ring-4 ring-red-500 rounded-full w-4 h-4 md:w-6 md:h-6 '></div>
+                  <div className='w-2 bg-black h-full'></div>
+                </div>
+
+                {/* right */}
+                <div className='h-full w-full border flex-1'></div>
+              </div>
             </div>
           </div>
           {/* image */}
