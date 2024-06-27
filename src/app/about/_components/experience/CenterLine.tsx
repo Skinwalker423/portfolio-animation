7b5cb6fa268ff1de488084da040e1ed12fa11d84
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-27 10:51:52 -0700

    responsive styles

--- src/app/about/_components/experience/CenterLine.tsx
+++ src/app/about/_components/experience/CenterLine.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const CenterLine = () => {
   return (
-    <div className='flex flex-col items-center mx-2 lg:mx-10 xl:mx-20 flex-shrink-0'>
+    <div className='flex flex-col items-center mx-3 lg:mx-10 xl:mx-20 flex-shrink-0'>
       <div className='bg-white ring-4 ring-red-500 rounded-full w-4 h-4 md:w-6 md:h-6 '></div>
       <div className='w-2 bg-black h-full'></div>
     </div>
--- src/app/about/_components/experience/ExperienceListItem.tsx
+++ src/app/about/_components/experience/ExperienceListItem.tsx
@@ -18,10 +18,12 @@ export const ExperienceListItem = ({
   return (
     <div className='flex flex-col pb-4 flex-1'>
       <div className='flex flex-col gap-6'>
-        <h2 className='bg-white text-black px-2 py-3 font-bold text-2xl rounded-lg text-center text-clip'>
+        <h2 className='bg-white text-black px-2 py-3 font-bold text-lg md:text-2xl rounded-lg text-center text-clip'>
           {title}
         </h2>
-        <p className='italic text-clip'>{description}</p>
+        <p className='italic text-clip text-sm md:text-base'>
+          {description}
+        </p>
         <span className='text-red-400'>{date}</span>
         <span>
           <Badge className='bg-white text-black hover:text-white hover:bg-black/60'>
