eec718b4f108f93541c22609afd98a92bb2a8e8f
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-01 09:09:43 -0700

    changed gradient for project card

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -9,7 +9,7 @@ import {
 import React, { useRef } from "react";
 import { projects } from "../../../constants";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
@@ -23,7 +23,7 @@ const PortfolioPage = () => {
   const x = useTransform(
     scrollYProgress,
     [0, 1],
-    ["14%", "-80%"]
+    ["11%", "-80%"]
   );
   return (
     <PageTransition>
@@ -36,33 +36,43 @@ const PortfolioPage = () => {
         </div>
         <div className='sticky top-0 flex h-screen gap-4 items-center overflow-hidden'>
           <motion.div style={{ x }} className='flex'>
-            <div className='h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300' />
-            {projects.map((item) => (
-              <div
-                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
-                key={item.id}
-              >
-                <div className='flex flex-col gap-8 text-white'>
-                  <h1 className='text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl'>
-                    {item.title}
-                  </h1>
-                  <div className='relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]'>
-                    <Image src={item.img} alt='' fill />
+            <div className='h-screen w-screen flex items-center justify-center bg-gradient-to-r from-cyan-300 to-orange-200' />
+            {projects.map((item, index) => {
+              console.log(item.color);
+              return (
+                <div
+                  className={cn(
+                    `h-screen w-screen flex items-center justify-center`,
+                    `bg-gradient-to-r ${
+                      index % 2 === 0
+                        ? "from-orange-200 to-cyan-300"
+                        : "from-cyan-300 to-orange-200"
+                    }`
+                  )}
+                  key={item.id}
+                >
+                  <div className='flex flex-col gap-8 text-white'>
+                    <h1 className='text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl'>
+                      {item.title}
+                    </h1>
+                    <div className='relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]'>
+                      <Image src={item.img} alt='' fill />
+                    </div>
+                    <p className='w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]'>
+                      {item.desc}
+                    </p>
+                    <Link
+                      href={item.link}
+                      className='flex justify-end'
+                    >
+                      <button className='p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded'>
+                        See Demo
+                      </button>
+                    </Link>
                   </div>
-                  <p className='w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]'>
-                    {item.desc}
-                  </p>
-                  <Link
-                    href={item.link}
-                    className='flex justify-end'
-                  >
-                    <button className='p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded'>
-                      See Demo
-                    </button>
-                  </Link>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </motion.div>
         </div>
       </div>
