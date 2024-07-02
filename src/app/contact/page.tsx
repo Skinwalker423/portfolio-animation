961f434cd8d7cf3bd40dfecae3b2c32b906d9a5f
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-02 11:56:21 -0700

    refactor

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,42 +1,16 @@
-"use client";
-
 import React from "react";
 import { PageTransition } from "@/components/PageTransition";
-import { motion } from "framer-motion";
 import { ContactForm } from "./_components/ContactForm";
+import { TypingText } from "@/components/animations/TypingText";
 
 const GREETING = "Say Hello!";
 
 const ContactPage = () => {
-  const splitGreeting = GREETING.split("");
-
   return (
     <PageTransition>
       <div className='h-[calc(100vh-6rem)] w-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 justify-center'>
-        <div className='flex flex-1 items-center justify-center text-4xl lg:text-6xl'>
-          <div className='flex gap-2'>
-            {splitGreeting.map((letter, index) => {
-              return (
-                <motion.span
-                  initial={{ opacity: 1 }}
-                  animate={{ opacity: 0 }}
-                  transition={{
-                    ease: "easeInOut",
-                    delay: index * 0.1,
-                    duration: 3,
-                    repeat: Infinity,
-                  }}
-                  key={index}
-                >
-                  {letter === " " ? <>&nbsp;</> : letter}
-                  <span>
-                    {splitGreeting.length - 1 === index &&
-                      "😊"}
-                  </span>
-                </motion.span>
-              );
-            })}
-          </div>
+        <div className='flex flex-1 items-center justify-center'>
+          <TypingText text={GREETING} />
         </div>
         <div className='flex w-full flex-1 items-center'>
           <ContactForm />
