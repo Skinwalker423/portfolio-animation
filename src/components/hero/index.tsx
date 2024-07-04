456f2084697baf9b02ef24e83487fb67444faf71
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-03 18:25:14 -0700

    new hero img

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -5,7 +5,7 @@ import { HeroTextBox } from "./HeroTextBox";
 export const HeroSection = () => {
   return (
     <div className='flex flex-col w-full h-[calc(100vh-8rem)] md:flex-row gap-1 sm:gap-10'>
-      <HeroImage src='/hero.png' alt='winter is coming' />
+      <HeroImage src='/hero4.png' alt='winter is coming' />
       <HeroTextBox />
     </div>
   );
