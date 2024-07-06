3507bc75f4aa2a1c18bdd588e05aad526584f57d
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-06 09:00:50 -0700

    tooltip for socials

--- src/components/navbar/Socials.tsx
+++ src/components/navbar/Socials.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { socials } from "../../../constants";
 import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export const Socials = () => {
   return (
     <div className='hidden md:flex gap-4 justify-end'>
       {socials.map(({ alt, href, src }) => {
         return (
-          <a target='_blank' key={alt} href={href}>
-            <Image
-              src={src}
-              alt={alt}
-              width={24}
-              height={24}
-            />
-          </a>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <a target='_blank' key={alt} href={href}>
+                  <Image
+                    src={src}
+                    alt={alt}
+                    width={24}
+                    height={24}
+                  />
+                </a>
+              </TooltipTrigger>
+              <TooltipContent className='mt-3'>
+                <p>{alt}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         );
       })}
     </div>
